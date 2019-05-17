fe04a7d05cf8b4f1e0e7bf08dd44ab3975020ffc
alumno <alumno@aula1-15>
2019-05-17 17:53:40 +0200

    login v14

--- A2Z/src/a2z/Pregunta.java
+++ A2Z/src/a2z/Pregunta.java
@@ -8,6 +8,7 @@ package a2z;
 import java.awt.Color;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.BorderFactory;
 
 /**
  *
@@ -24,7 +25,8 @@ public class Pregunta extends javax.swing.JFrame {
         this.setTitle("A2Z Game");
         this.getContentPane().setBackground(Color.decode("#1f2029"));
         jLabel2.setForeground(Color.decode("#fafafa"));
-        jLabelpregunta.setForeground(Color.decode("#fafafa"));
+        jTextArea1.setBackground(Color.decode("#1f2029"));
+        jTextArea1.setForeground(Color.decode("#fafafa"));
 
         jButtonRespuesta1.setForeground(Color.BLACK);
         jButtonRespuesta1.setBackground(Color.WHITE);
@@ -34,10 +36,16 @@ public class Pregunta extends javax.swing.JFrame {
 
         jButtonRespuesta3.setForeground(Color.BLACK);
         jButtonRespuesta3.setBackground(Color.WHITE);
+        jTextArea1.setLineWrap(true);
+        jTextArea1.setEditable(false);
+        jTextArea1.setText("¿Por qué razón “separado” se escribe todo junto y “todo junto” se escribe separado?");
 
+    }
+
+    private void tiempo() {
         boolean bandera = true;
 
-        while (bandera != true) {
+        while (bandera == true) {
 
             if (jProgressBar1.getValue() == 1200) {
                 bandera = false;
@@ -46,13 +54,12 @@ public class Pregunta extends javax.swing.JFrame {
                 this.dispose();
             } else {
                 bandera = true;
-                tiempo();
+                dormir();
             }
         }
-
     }
 
-    private void tiempo() {
+    private void dormir() {
         try {
             Thread.sleep(80);
             int tiempo = 0;
@@ -71,19 +78,16 @@ public class Pregunta extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabelpregunta = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
         jButtonRespuesta1 = new javax.swing.JButton();
         jButtonRespuesta2 = new javax.swing.JButton();
         jProgressBar1 = new javax.swing.JProgressBar();
         jLabel2 = new javax.swing.JLabel();
         jButtonRespuesta3 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabelpregunta.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabelpregunta.setText("jLabel1");
-        jLabelpregunta.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
         jButtonRespuesta1.setText("Respuesta 1");
 
@@ -95,6 +99,11 @@ public class Pregunta extends javax.swing.JFrame {
 
         jButtonRespuesta3.setText("Respuesta 3");
 
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Monospaced", 0, 16)); // NOI18N
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -102,12 +111,10 @@ public class Pregunta extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE)
                     .addComponent(jSeparator1)
                     .addComponent(jButtonRespuesta1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jButtonRespuesta2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabelpregunta, javax.swing.GroupLayout.PREFERRED_SIZE, 247, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 8, Short.MAX_VALUE))
                     .addComponent(jButtonRespuesta3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2)
@@ -123,8 +130,8 @@ public class Pregunta extends javax.swing.JFrame {
                     .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabelpregunta, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(9, 9, 9)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jButtonRespuesta1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -132,7 +139,7 @@ public class Pregunta extends javax.swing.JFrame {
                 .addComponent(jButtonRespuesta2, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(37, 37, 37)
                 .addComponent(jButtonRespuesta3, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(43, Short.MAX_VALUE))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
 
         pack();
@@ -178,8 +185,9 @@ public class Pregunta extends javax.swing.JFrame {
     private javax.swing.JButton jButtonRespuesta2;
     private javax.swing.JButton jButtonRespuesta3;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabelpregunta;
     private javax.swing.JProgressBar jProgressBar1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
